86465a674a5c230c27ecbfb55d143a0712461945
DevelopJKong <ybaram40@gmail.com>
2022-04-22 20:12:46 +0900

    contact 완료

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 const header = document.querySelector("header");
 const headerHeight = header.getBoundingClientRect().height;
+const portfolio = document.querySelector("#portfolio");
+const portfolioTitles = portfolio.querySelectorAll(".portfolio__title");
+
+portfolioTitles.forEach((item) => {
+  item.innerText.length > 20
+    ? (item.innerText = `${item.innerText.slice(0, 20)}...`)
+    : item.innerText;
+});
 
 function onScroll() {
   if (window.scrollY > headerHeight) {
